733e12902afd2b74b2629d4408f0cc4f8209042b
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-11 11:43:59 +0100

    init x and y as static variables

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -2,22 +2,26 @@ package org.example;
 
 public class PlayerCharacter {
 
+    static int X = 0;
+    static int Y = 0;
+
     public static int getX() {
-        return 0;
+        return X;
     }
 
     public static int getY() {
-        return 0;
+        return Y;
     }
 
     public static boolean move(String input) {
-        int Y = 0;
         if (input.equals("W")) {
             Y = Y+1;
        }
 
-        if (Y==1 ) {
+        if (Y==1) {
             return true;
-        } return false;
+        }
+
+        return false;
     }
 }
